966804f54d78f07c7b9c28e57a0ae08850d947fe
Aurii <49456123+seenks1@users.noreply.github.com>
2020-07-10 01:11:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,38 @@ const client = new Discord.Client();
 const channelID = process.env.CHANNEL_ID;
 const token = process.env.DISCORD_APP_TOKEN;
 
+const getNonDeafMemberCount = channel => {
+    if (!channel.members) return 0;
+    return channel.members.reduce((count, member) => count + +!member.deaf, 0);
+}
+
 client.on('ready', async () => {
+
     const channel = await client.channels.fetch(channelID);
     if (channel.type !== 'voice') {
         return;
     }
 
     const connection = await channel.join();
-    const dispatcher = connection.play('http://kpop.bigbradio.net/');
+    if (connection.dispatcher) connection.dispatcher.end();
+
+    let dispatcher = connection.play('http://kpop.bigbradio.net/');
+    const onDispatcherFinish = () => {
+        if (connection.dispatcher) return;
+        if (dispatcher) dispatcher.end();
+        dispatcher = connection.play('http://kpop.bigbradio.net/');
+        dispatcher.on('finish', onDispatcherFinish);
+    };
+    dispatcher.on('finish', onDispatcherFinish);
+
     const onChannelChange = () => {
-        if (channel.members.size <= 1) dispatcher.pause();
-        else if (dispatcher.paused) dispatcher.resume();
+        if (getNonDeafMemberCount(channel) <= 1) { // Bot itself is not deaf, that's why 1 instead of 0
+            console.log('Not enough members', getNonDeafMemberCount(channel), 'Pausing...');
+            dispatcher.pause();
+        } else if (dispatcher.paused) {
+            console.log('Continuing...');
+            dispatcher.resume();
+        }
     };
 
     onChannelChange();
